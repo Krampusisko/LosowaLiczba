e4db20ea5c2f2260336021558fe886540c9d538b
user <user@DESKTOP-9J61BBV>
2022-03-10 13:41:02 +0100

    Dodaj pliki projektów.

---
+++ LosowaLiczba/Program.cs
@@ -0,0 +1,32 @@
+﻿Random r = new Random();
+int kod = r.Next(0, 10);
+double a;
+Console.WriteLine("zgadnij liczbe");
+a = double.Parse(Console.ReadLine());
+if (a < kod)
+{
+    Console.WriteLine("Podana liczba jest mniejsza od tej ktora wylosowales");
+}
+else if (a > kod)
+{
+    Console.WriteLine("Podana liczba jest wieksza od tej ktora wylosowales");
+
+}
+else
+    Console.WriteLine("Gratulacje zgadles");
+while (a != kod)
+{
+    Console.WriteLine("zgadnij liczbe");
+    a = double.Parse(Console.ReadLine());
+    if (a < kod)
+    {
+        Console.WriteLine("Podana liczba jest mniejsza od tej ktora wylosowales");
+    }
+    else if (a > kod)
+    {
+        Console.WriteLine("Podana liczba jest wieksza od tej ktora wylosowales");
+
+    }
+    else
+        Console.WriteLine("Gratulacje zgadles");
+}
